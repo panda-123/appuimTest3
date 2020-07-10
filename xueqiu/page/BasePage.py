259fe300db306c185589f1e2946c2c0726125e28
panda <herui5555@126.com>
2020-07-10 18:45:52 +0800

    增加黑名单

--- xueqiu/page/BasePage.py
+++ xueqiu/page/BasePage.py
@@ -10,7 +10,7 @@ from lxml import etree
 class BasePage(object):
     # 黑名单，处理随时可能的弹窗
     blank_words = ["//*[text='好的']"]
-    def find(self,by=By.ID, value = None):
+    def findBy(self,by=By.ID, value = None):
         try:
             return Appium.driver.find_element(by, value)
         except:
@@ -22,9 +22,14 @@ class BasePage(object):
                 if(len(xml.xpath(w))>0):
                     Appium.driver.find_element(By.XPATH, w).click()
 
-
-
-
-
-    def find(self,locate):
-        Appium.driver.find_element(*locate)
+    def find(self,by,value):
+        try:
+            return Appium.driver.find_element(by,value)
+        except:
+            page_source = Appium.driver.page_source
+            print(page_source)
+            xml = etree.XML(page_source)
+            for w in self.blank_words:
+                print(w)
+                if(len(xml.xpath(w))>0):
+                    Appium.driver.find_element(By.XPATH, w).click()
